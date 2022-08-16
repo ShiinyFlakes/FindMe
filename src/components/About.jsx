f1cb65e9ba386ec84d30b20c8a193f11aa2edd01
Mak513 <osei.makangu@hotmail.com>
2022-08-16 16:04:48 +0200

    fertig

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,11 @@
 import { Button, Box, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Tab, Tabs, Toolbar} from "@mui/material";
+import { AppBar, Tab, Tabs, Toolbar } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import "./about.css";
 import Freunde from "../../src/images/Freunde.jpg";
-
 
 const About = () => {
   const [value, setValue] = useState();
@@ -14,49 +13,67 @@ const About = () => {
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.reload();
-};
+  };
 
   return (
-    <div style={{ backgroundImage: `url(${Freunde})`,
-    backgroundPosition: "center", backgroundSize:"cover" }}>
-    <div>
-      <nav className="navbar">
-      <a style={{textDecoration: "none"}} href="/">
-      <h1>FindMe</h1></a>
-                <button className="white_btn" onClick={handleLogout}>
-                Abmelden
-                </button>
-            </nav>
-            <AppBar sx={{ backgroundColor: "#3bb19b"}} position="sticky">
-        <Toolbar >
-          <Tabs
-            sx={{ ml: "auto" }}
-            textColor="inherit"
-            indicatorColor="white"
-            value={1}
-            onChange={(e, val) => setValue(val)}
+    <div
+      style={{
+        backgroundImage: `url(${Freunde})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div>
+        <nav className="navbar">
+          <a style={{ textDecoration: "none" }} href="/">
+            <h1>FindMe</h1>
+          </a>
+          <button className="white_btn" onClick={handleLogout}>
+            Abmelden
+          </button>
+        </nav>
+        <AppBar sx={{ backgroundColor: "#3bb19b" }} position="sticky">
+          <Toolbar>
+            <Tabs
+              sx={{ ml: "auto" }}
+              textColor="inherit"
+              indicatorColor="white"
+              value={1}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab
+                LinkComponent={NavLink}
+                to="/add"
+                label="Aktivität erstellen"
+              />
+              <Tab
+                LinkComponent={NavLink}
+                to="/activitys"
+                label="Aktivitäten"
+              />
+              <Tab LinkComponent={NavLink} to="/about" label="Über uns" />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <div className="divBox">
+            <Typography
+              sx={{ fontFamily: "Oxygen", color: "whitesmoke" }}
+              variant="h2"
+            >
+              FindUs Kawan Osei Mago
+            </Typography>
+          </div>
+          <Button
+            LinkComponent={Link}
+            to="/activitys"
+            sx={{ marginTop: 15, background: "#3bb19b" , color: "white" }}
+            variant="primary"
           >
-            <Tab LinkComponent={NavLink} to="/add" label="Aktivität erstellen" />
-            <Tab LinkComponent={NavLink} to="/activitys" label="Aktivitäten" />
-            <Tab LinkComponent={NavLink} to="/about" label="Über uns" />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-      <Box  display="flex" flexDirection="column" alignItems="center">
-        <Typography  sx={{ fontFamily: "fantasy", color: "whitesmoke" }} variant="h2">
-          Full-Stack MERN Application
-          By Kawan Osei Mago
-        </Typography>
-        <Button
-          LinkComponent={Link}
-          to="/activitys"
-          sx={{ marginTop: 15, background: "#3bb19b" }}
-          variant="primary"
-        >
-          <Typography variant="h3">Zu den Aktivitäten</Typography>
-        </Button>
-      </Box>
-    </div>
+            <Typography variant="h3">Zu den Aktivitäten</Typography>
+          </Button>
+        </Box>
+      </div>
     </div>
   );
 };
